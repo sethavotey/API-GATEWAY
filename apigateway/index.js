2b1ea1c2af6602e9d375f187c951b7e9bd88dcd7
sethavotey <151896519+sethavotey@users.noreply.github.com>
2025-05-30 16:09:06 +0700

    Update index.js

--- apigateway/index.js
+++ apigateway/index.js
@@ -3,10 +3,10 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
 // Route for Student Service
-app.use('/student', createProxyMiddleware({ target: 'http://localhost:5001', changeOrigin: true }));
+app.use('/student', createProxyMiddleware({ target: 'http://54.86.84.18:5001', changeOrigin: true }));
 
 // Route for Teacher Service
-app.use('/teacher', createProxyMiddleware({ target: 'http://localhost:5002', changeOrigin: true }));
+app.use('/teacher', createProxyMiddleware({ target: 'http://54.175.16.72:5002', changeOrigin: true }));
 
 app.listen(5000, () =>
     console.log('API Gateway running on Port No: 5000'));
